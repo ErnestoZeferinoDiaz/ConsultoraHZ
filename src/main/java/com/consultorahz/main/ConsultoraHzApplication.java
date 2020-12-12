f20e0b48ee262c6c5f0dfe4d197ff35a784eb72b
Ernesto Zeferino Diaz <ernesto.zefe.d@gmail.com>
2020-12-12 12:32:29 -0600

    First Commit

---
+++ src/main/java/com/consultorahz/main/ConsultoraHzApplication.java
@@ -0,0 +1,27 @@
+package com.consultorahz.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class ConsultoraHzApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ConsultoraHzApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+            }
+        };
+    }
+
+}
---
+++ src/test/java/com/consultorahz/main/ConsultoraHzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultorahz.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultoraHzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
