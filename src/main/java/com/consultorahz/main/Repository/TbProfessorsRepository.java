d10a993017313c554e1ad58cfc60195e0af00213
Ernesto Zeferino Diaz <ernesto.zefe.d@gmail.com>
2020-12-12 14:14:38 -0600

    Creacion de Servicio TbProfessorsService y su implementacion

--- src/main/java/com/consultorahz/main/Repository/TbProfessorsRepository.java
+++ src/main/java/com/consultorahz/main/Repository/TbProfessorsRepository.java
@@ -2,6 +2,8 @@ package com.consultorahz.main.Repository;
 
 import com.consultorahz.main.Entity.TbProfessorsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TbProfessorsRepository extends JpaRepository<TbProfessorsEntity, Long> {
 }
---
+++ src/main/java/com/consultorahz/main/Service/ITbProfessorsService.java
@@ -0,0 +1,10 @@
+package com.consultorahz.main.Service;
+
+import com.consultorahz.main.Entity.TbProfessorsEntity;
+import java.util.List;
+
+public interface ITbProfessorsService {
+    public List<TbProfessorsEntity> buscarTodos();
+    public TbProfessorsEntity buscarPorId(Long id);
+    public TbProfessorsEntity guardar(TbProfessorsEntity pe);
+}
