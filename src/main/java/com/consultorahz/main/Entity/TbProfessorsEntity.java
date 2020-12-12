5673bff28d3402d17036512e3a052fddf7e52634
Ernesto Zeferino Diaz <ernesto.zefe.d@gmail.com>
2020-12-12 13:59:40 -0600

    Creacion de Repositorio TbProfessorRepository

--- src/main/java/com/consultorahz/main/Entity/TbProfessorsEntity.java
+++ src/main/java/com/consultorahz/main/Entity/TbProfessorsEntity.java
@@ -1,7 +1,6 @@
 package com.consultorahz.main.Entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
---
+++ src/main/java/com/consultorahz/main/Repository/TbProfessorsRepository.java
@@ -0,0 +1,7 @@
+package com.consultorahz.main.Repository;
+
+import com.consultorahz.main.Entity.TbProfessorsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TbProfessorsRepository extends JpaRepository<TbProfessorsEntity, Long> {
+}
